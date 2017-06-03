99e4efc03381e31b464a6ee870277e4c49f782e6
Ammar Yasser <plus.ammaryasser@gmail.com>
2017-06-04 00:04:54 +0200

    test

--- app/src/main/java/com/example/ammaryasser/portsaidtourguide/card.java
+++ app/src/main/java/com/example/ammaryasser/portsaidtourguide/card.java
@@ -1,8 +1,74 @@
 package com.example.ammaryasser.portsaidtourguide;
 
-/**
- * Created by Ammar Yasser on 6/3/2017.
- */
 
-public class card {
+public class Card {
+
+    private String placeName;
+    private String placeDescription;
+    private int placeOpenTime;
+    private int placeCloseTime;
+    private int placeImageRId;
+
+    /**
+     * Create a new Card object with the name, description, open, close times and a photo of the
+     * place
+     *
+     * @param placeName        is used to set the place name for the word object.
+     * @param placeDescription is used to set the description text that appear in the bottom of the
+     *                         card
+     * @param placeOpenTime    is used to set the time the place is supposed to open in 24-hour format
+     * @param placeCloseTime   is used to set the time the place is supposed to close in 24-hour
+     *                         format
+     * @param placeImageRId    is used to set the image for the place in the card
+     */
+
+    public Card(String placeName, String placeDescription, int placeOpenTime, int placeCloseTime,
+                int placeImageRId) {
+        this.placeName = placeName;
+        this.placeDescription = placeDescription;
+        this.placeOpenTime = placeOpenTime;
+        this.placeCloseTime = placeCloseTime;
+        this.placeImageRId = placeImageRId;
+    }
+
+    public String getPlaceName() {
+        return placeName;
+    }
+
+    public void setPlaceName(String placeName) {
+        this.placeName = placeName;
+    }
+
+    public String getPlaceDescription() {
+        return placeDescription;
+    }
+
+    public void setPlaceDescription(String placeDescription) {
+        this.placeDescription = placeDescription;
+    }
+
+    public int getPlaceOpenTime() {
+        return placeOpenTime;
+    }
+
+    public void setPlaceOpenTime(int placeOpenTime) {
+        this.placeOpenTime = placeOpenTime;
+    }
+
+    public int getPlaceCloseTime() {
+        return placeCloseTime;
+    }
+
+    public void setPlaceCloseTime(int placeCloseTime) {
+        this.placeCloseTime = placeCloseTime;
+    }
+
+    public int getPlaceImageRId() {
+        return placeImageRId;
+    }
+
+    public void setPlaceImageRId(int placeImageRId) {
+        this.placeImageRId = placeImageRId;
+    }
 }
+
