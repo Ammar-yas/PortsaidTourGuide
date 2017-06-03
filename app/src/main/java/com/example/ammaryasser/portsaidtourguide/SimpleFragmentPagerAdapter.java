bfe2b5f18942a6a8dcdde5ec0f3b33eeb58ad0d5
Ammar Yasser <plus.ammaryasser@gmail.com>
2017-06-03 08:40:11 +0200

    test

---
+++ app/src/main/java/com/example/ammaryasser/portsaidtourguide/SimpleFragmentPagerAdapter.java
@@ -0,0 +1,40 @@
+package com.example.ammaryasser.portsaidtourguide;
+
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class SimpleFragmentPagerAdapter extends FragmentPagerAdapter {
+
+    private final String TABTITLES[] = new String[]{"Attractions", "Entertain", "Relax", "Restaurants"};
+
+    public SimpleFragmentPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        if (position == 0) {
+            return new AttractionsFragment();
+        } else if (position == 1) {
+            return new EntertainFragment();
+        } else if (position == 2) {
+            return new RelaxFragment();
+        } else {
+            return new RestaurantsFragment();
+        }
+
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return TABTITLES[position];
+    }
+}
