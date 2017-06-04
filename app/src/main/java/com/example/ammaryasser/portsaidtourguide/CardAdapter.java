b1af97312866f0a500d22adb661c0d0831ee1d2f
Ammar Yasser <plus.ammaryasser@gmail.com>
2017-06-04 05:15:35 +0200

    test

--- app/src/main/java/com/example/ammaryasser/portsaidtourguide/CardAdapter.java
+++ app/src/main/java/com/example/ammaryasser/portsaidtourguide/CardAdapter.java
@@ -3,6 +3,7 @@ package com.example.ammaryasser.portsaidtourguide;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -19,6 +20,15 @@ public class CardAdapter extends ArrayAdapter<Card> {
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
 
+        View cardView = convertView;
+
+        if ( convertView == null ){
+            cardView = LayoutInflater.from(getContext()).inflate(R.layout.card_view, parent, false);
+        }
+
+        Card card = getItem(position);
+
+        //TODO: link the card View with the card class then then return the cardView variable
 
         return super.getView(position, convertView, parent);
     }
